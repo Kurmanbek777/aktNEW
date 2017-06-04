71dc4814590ce8d5961ab0c0cfa8588f55c5cef8
Kurmanbek777 <knurkamil@gmail.com>
2017-06-04 16:07:05 +0600

    new akt
    новый проект

---
+++ target/generated-sources/annotations/com/test/akt8/Users_.java
@@ -0,0 +1,26 @@
+package com.test.akt8;
+
+import com.test.akt8.Departments;
+import com.test.akt8.Employees;
+import com.test.akt8.Inventoryhelper;
+import com.test.akt8.Offices;
+import com.test.akt8.Transfer;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-04T15:21:16")
+@StaticMetamodel(Users.class)
+public class Users_ { 
+
+    public static volatile CollectionAttribute<Users, Employees> employeesCollection;
+    public static volatile SingularAttribute<Users, String> password;
+    public static volatile CollectionAttribute<Users, Offices> officesCollection;
+    public static volatile SingularAttribute<Users, Integer> id;
+    public static volatile CollectionAttribute<Users, Departments> departmentsCollection;
+    public static volatile SingularAttribute<Users, String> login;
+    public static volatile CollectionAttribute<Users, Transfer> transferCollection;
+    public static volatile CollectionAttribute<Users, Inventoryhelper> inventoryhelperCollection;
+
+}
